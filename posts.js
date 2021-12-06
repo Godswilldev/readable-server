ae1d8a1188da424503b6f754081ee0eebd1cb386
Godswill Edet <gimmex1@gmail.com>
2021-12-06 19:59:42 +0100

    add more posts and comments

--- posts.js
+++ posts.js
@@ -8,12 +8,11 @@ const defaultData = {
     timestamp: 1467166872634,
     title: "Udacity is the best place to learn React",
     body: "Everyone says so after all.",
-    author: "thingtwo",
+    author: "John Doe",
     category: "react",
     voteScore: 6,
     deleted: false,
     commentCount: 2,
-    status: "planned",
   },
 
   "6ni6ok3ym7mf1p33lnez": {
@@ -21,12 +20,35 @@ const defaultData = {
     timestamp: 1468479767190,
     title: "Learn Redux in 10 minutes!",
     body: "Just kidding. It takes more than 10 minutes to learn technology.",
-    author: "thingone",
+    author: "Sarah Edo",
     category: "redux",
     voteScore: -5,
     deleted: false,
     commentCount: 0,
-    status: "in-progress",
+  },
+
+  "3426ok3ym7mf1p33lnez": {
+    id: "3426ok3ym7mf1p33lnez",
+    timestamp: 12368479767190,
+    title: "Nigeria",
+    body: "Nigeria is the largest country by population in africa",
+    author: "Sarah Edo",
+    category: "redux",
+    voteScore: 3,
+    deleted: false,
+    commentCount: 0,
+  },
+
+  "5edf6ok3ym7mf1p33lnez": {
+    id: "5edf6ok3ym7mf1p33lnez",
+    timestamp: 38768479767190,
+    title: "Best Framework/Library",
+    body: "React is the best Frontend framework",
+    author: "Sarah Edo",
+    category: "react",
+    voteScore: 8,
+    deleted: false,
+    commentCount: 0,
   },
 
   "3jd6ok3ym7mf1p33lysd": {
@@ -34,12 +56,11 @@ const defaultData = {
     timestamp: 1468479767192,
     title: "Update Udacity",
     body: "Update Udacity's react course content to use Hooks",
-    author: "thingtwo",
+    author: "John Doe",
     category: "udacity",
     voteScore: 4,
     deleted: false,
     commentCount: 1,
-    status: "live",
   },
 };
 
